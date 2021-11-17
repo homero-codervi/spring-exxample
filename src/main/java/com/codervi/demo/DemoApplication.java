d962f6df11d5e3f1bd81a50b2f51444e57d8f7fb
Homero Villanueva <homero.codervi@gmail.com>
2021-11-17 15:55:42 -0600

    Test 2

--- src/main/java/com/codervi/demo/DemoApplication.java
+++ src/main/java/com/codervi/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello Codervi.com");
 		System.out.println("Hello Test");
+		System.out.println("Hello Test2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
