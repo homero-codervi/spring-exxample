fab07720bb688f4cb2cd2992aeb267bb510ca70c
Homero Villanueva <homero.codervi@gmail.com>
2021-11-17 11:42:14 -0600

    The first commit with the original project

---
+++ src/test/java/com/codervi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codervi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
